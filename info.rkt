6db6cfc77197197470317dfb26d928f41026a182
J. Ian Johnson <ianj@ccs.neu.edu>
2013-11-05 10:42:53 -0500

    Talks now a Racket package

---
+++ info.rkt
@@ -0,0 +1,16 @@
+#lang setup/infotab
+(define name "ianj-talks")
+(define version "1.0")
+(define deps '("base" "slideshow-lib"))
+(define setup-collects (list "boyer-moore"
+                             "hopa2013"
+                             "icfp2013"
+                             "IBMplday"
+                             "milawa"
+                             "nasa-priorities"
+                             "phdsemCFA"
+                             "phdsemSMT"
+                             "smt-pl"
+                             "proposal"
+                             "utils"))
+(define collection 'multi)
